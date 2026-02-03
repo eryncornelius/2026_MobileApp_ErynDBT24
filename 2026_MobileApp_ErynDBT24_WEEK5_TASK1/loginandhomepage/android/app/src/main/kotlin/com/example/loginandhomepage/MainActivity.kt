1879c980ab7d66c0d4ee68c4028ecdc8d08737f1
eryncornelius <eryncornelius888@gmail.com>
2026-02-03 10:54:51 +0700

    push task 1

---
+++ 2026_MobileApp_ErynDBT24_WEEK5_TASK1/loginandhomepage/android/app/src/main/kotlin/com/example/loginandhomepage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loginandhomepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ 2026_MobileApp_ErynDBT24_WEEK5_TASK1/loginandhomepage/lib/homepage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> pages = const [
+    Center(child: Text('Welcome!', style: TextStyle(fontSize: 20))),
+    Center(child: Text('Book Now!', style: TextStyle(fontSize: 20))),
+    Center(child: Text('My Profile', style: TextStyle(fontSize: 20))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Book',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
